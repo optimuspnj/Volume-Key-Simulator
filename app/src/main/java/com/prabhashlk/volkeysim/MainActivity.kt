f82c981a6cb6a16533ed92f704659a9fa53e412e
Prabhash Jayaweera <prabhash_2018392@fot.ruh.ac.lk>
2022-07-27 13:02:45 +0530

    Add: Comments Add: Proper versioning

--- app/src/main/java/com/prabhashlk/volkeysim/MainActivity.kt
+++ app/src/main/java/com/prabhashlk/volkeysim/MainActivity.kt
@@ -12,29 +12,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Test buttons declaration
         val upBtn: Button = findViewById(R.id.btnUp)
         val downBtn: Button = findViewById(R.id.btnDown)
 
+        //Audio service declaration for using audio manager
         val audioManager = applicationContext.getSystemService(AUDIO_SERVICE) as AudioManager
 
+        //Setup onclick listener to volume up button
         upBtn.setOnClickListener {
+            //Use audio manager to raise volume and show UI audio panel
             audioManager.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI)
         }
 
+        //Setup onclick listener to volume down button
         downBtn.setOnClickListener {
+            //Use audio manager to lower volume and show UI audio panel
             audioManager.adjustVolume(AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI)
         }
 
+        //Textview for github link - Making it clickable
         val gitLink: TextView = findViewById(R.id.textView_link)
         gitLink.autoLinkMask
     }
 }
 
+//Class for Quick Setting Tile activity - The main purpose of the app
 class VolSimService : TileService() {
 
+    //Overriding tile onclick activity
     override fun onClick() {
         super.onClick()
+        //Audio service declaration for using audio manager
         val audioManager = applicationContext.getSystemService(AUDIO_SERVICE) as AudioManager
+        //Use audio manager to raise volume and show UI audio panel
         audioManager.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI)
     }
 }
